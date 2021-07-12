8553e2d2a3b10902ebbbec037eab25df279f4fee
Brandon <bjkrynicki@gmail.com>
2021-07-12 03:47:46 -0500

    commit tree 659272ca40cc9f13e43e6e1d723c49356c1a354c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+function superbowlWin(yearArray) {
+  let yearWin = yearArray.find((element) => element.result === `W`);
+  if (yearWin) {
+    return yearWin.year;
+  } else return undefined;
+}
+superBowlWin(record);
